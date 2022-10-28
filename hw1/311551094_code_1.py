ecb99335b89ae38aa7945356ea8e4a9fd5c179dc
YuWei Liao <awei6209@gmail.com>
2022-10-28 17:40:43 +0800

    initialize independent set

--- hw1/311551094_code_1.py
+++ hw1/311551094_code_1.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-#add node weight to the WS model
+#add random node weight to the WS model
 def add_node_weight(G):
     weights = {}
     for i in range(30):
@@ -24,6 +24,15 @@ def calculate_node_priority(G):
 
     return priorities
 
+#randomly initialize independent set
+def initialize_independent_set(G):
+    set = {}
+    for i in range(30):
+        set[i] = random.randint(0,1)
+    nx.set_node_attributes(G, set, name='independent_set')
+
+    return set
+
 #print graph
 def print_graph(G):
     labels = {n: str(n) + ': ' + str(G.nodes[n]['weight']) for n in G.nodes}
@@ -39,7 +48,7 @@ G = nx.watts_strogatz_graph(n = 30, k = 4, p = 0.5)
 adjacency = nx.to_numpy_array(G)
 node_weight = add_node_weight(G)
 node_priority = calculate_node_priority(G)
+independent_set = initialize_independent_set(G)
 
 
-
-print_graph(G)
+#print_graph(G)
